2584a02c4d1f5ce8132a16f78cf85f9c537c4827
Sara <moniquesarah5@gmail.com>
2025-01-28 14:51:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,17 @@
 $(document).ready(function () {
     console.log(document.querySelector('header button'))
     console.log($('button-cancelar'))
+
+
+    //primeiro opção
+    $('header button').click(function(){
+        alert('Tudo certo bebê!')
+    })
+     //segunda opção
+
+     $('form').on('submit', function(e){
+        e.preventDefault()
+     })
+
+
 });
