b9337eb8c23f210ed485dd9dbf0f948b527cc93e
Sara <moniquesarah5@gmail.com>
2025-02-03 15:22:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,6 +15,23 @@ $(document).ready(function () {
 
      $('form').on('submit', function(e){
         e.preventDefault()
+
+
+        const enderecoNovaImagem = $('#endereco-imagem').val()
+
+
+        const novoItem = $('<li style = "display:none"></li>')
+
+
+        $(`<img src="${enderecoNovaImagem}"/>`).appendTo(novoItem)
+
+
+        $(`<div class="overlay"><a href="${enderecoNovaImagem}" target = "_blank" title="Ver imagem em tamanho real>Ver imagem em tamanho real</a></div>`).appendTo(novoItem)
+
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1000)
+
+        $('#endereco-imagem').value('')
      })
 
 
